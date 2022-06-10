cbbe23bc0dc9bb10db094da48e9d7c72e40c2898
Franck Anso <franck.anso@senioradom.com>
2022-06-10 10:44:47 +0200

    Full project.

---
+++ src/main/java/com/krgcorporate/grpc/HelloServiceImpl.java
@@ -0,0 +1,28 @@
+package com.krgcorporate.grpc;
+
+import com.krgcorporate.grpc.HelloServiceGrpc.HelloServiceImplBase;
+
+import io.grpc.stub.StreamObserver;
+
+public class HelloServiceImpl extends HelloServiceImplBase {
+
+    @Override
+    public void hello(
+            HelloRequest request,
+            StreamObserver<HelloResponse> responseObserver
+    ) {
+        System.out.println("Request received from client:\n" + request);
+
+        String greeting = "Hello, " +
+                request.getFirstName() +
+                " " +
+                request.getLastName();
+
+        HelloResponse response = HelloResponse.newBuilder()
+            .setGreeting(greeting)
+            .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
